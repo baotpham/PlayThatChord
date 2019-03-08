683b3cadfbe2c06df3699f4b737fadd7f2d509f6
Bao Thien Pham <baotpham7@gmail.com>
2019-03-08 00:56:13 -0500

    added about and help menu

--- src/com/playthatchord/controller/MenuController.java
+++ src/com/playthatchord/controller/MenuController.java
@@ -8,6 +8,7 @@ public class MenuController implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        System.out.println("Command: " + e.getActionCommand());
 
         switch (e.getActionCommand()) {
             case "Exit":
@@ -23,8 +24,51 @@ public class MenuController implements ActionListener {
                     System.exit(0);
                 }
 
+                break;
+
+            case "About":
+                JOptionPane.showMessageDialog(null,
+                        "Play that Chord \nDeveloped by Bao Pham \nv1.0",
+                        "About",
+                        JOptionPane.PLAIN_MESSAGE);
+                break;
+
+            case "Help":
+                Object[] possibilities = {"What is PlayThatChord?", "How to Use"};
+
+                String s = (String)JOptionPane.showInputDialog(
+                        null,
+                        "What do you need help with?",
+                        "Help",
+                        JOptionPane.PLAIN_MESSAGE,
+                        null,
+                        possibilities,
+                        "What is PlayThatChord?");
+
+                if (( s != null ) && (s.length() > 0)) {
+                    if(s.equals("What is PlayThatChord?")) {
+                        JOptionPane.showMessageDialog(null,
+                                "PlayThatChord is a software that allows you find chord shapes on a 6-string guitar." +
+                                        "\nIt also shows you different variations of chord shapes along with fingering.",
+                                "What is PlayThatChord?",
+                                JOptionPane.PLAIN_MESSAGE);
+
+                    } else if (s.equals("How to Use")) {
+                        JOptionPane.showMessageDialog(null,
+                                "You simply select a chord button on top of the window."
+                                        + "\nPlayThatChord will shows you chord structure, chord shapes, chord fingering, and chord description."
+                                        + "\nYou can also use Chord Option area on the right of the screen for different settings",
+                                "How to Use",
+                                JOptionPane.PLAIN_MESSAGE);
+                    }
+                }
+
+                break;
+
             default:
                 System.out.println("ERROR");
         }
+
+        System.out.println();
     }
 }
--- src/com/playthatchord/view/MenuView.java
+++ src/com/playthatchord/view/MenuView.java
@@ -12,14 +12,28 @@ public class MenuView extends JMenuBar{
         JMenu fileMenu = new JMenu("File");
         JMenu helpMenu = new JMenu("Help");
 
-        add(fileMenu);
-        add(helpMenu);
 
+        /* file menu */
         JMenuItem exitItem = new JMenuItem("Exit");
         exitItem.addActionListener(new MenuController());
 
-
         fileMenu.add(exitItem);
+
+
+        /* help menu */
+        JMenuItem aboutItem = new JMenuItem("About");
+        aboutItem.addActionListener(new MenuController());
+
+        JMenuItem helpItem = new JMenuItem("Help");
+        helpItem.addActionListener(new MenuController());
+
+
+        helpMenu.add(aboutItem);
+        helpMenu.add(helpItem);
+
+
+        add(fileMenu);
+        add(helpMenu);
     }
 
 }
